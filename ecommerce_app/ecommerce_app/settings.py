b722f187ff1f2c51541f7bb2eb9601c28e391b1d
brianstatesthat <mbspeelman@gmail.com>
2025-09-30 15:38:40 +0200

    added users app to the settings py file

--- ecommerce_app/ecommerce_app/settings.py
+++ ecommerce_app/ecommerce_app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mystore'
+    'mystore',
+    'users'
 ]
 
 MIDDLEWARE = [
