255199cc4ef3480c58cb8969b36ed5eaca08ba84
brianstatesthat <mbspeelman@gmail.com>
2025-10-12 14:09:00 +0200

    added permissions to mystore views which has the products

--- ecommerce_app/mystore/models.py
+++ ecommerce_app/mystore/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock_quantity = models.PositiveIntegerField()
+    image_url = models.URLField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
--- ecommerce_app/mystore/views.py
+++ ecommerce_app/mystore/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_date')
+    serializer_class = ProductSerializer
+     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['name', 'category__name']
+    filterset_fields = ['category', 'price', 'stock_quantity']
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
