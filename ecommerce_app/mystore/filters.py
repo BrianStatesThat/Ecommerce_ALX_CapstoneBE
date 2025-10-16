63944bc48e3a51dddcb2d530e4958103e1bcba73
brianstatesthat <mbspeelman@gmail.com>
2025-10-16 22:57:03 +0200

    Implemented more filters and added user login and register and updated the readme file accordingly also made migrations to implement the reviews

--- ecommerce_app/mystore/filters.py
+++ ecommerce_app/mystore/filters.py
@@ -5,10 +5,12 @@ class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
     in_stock = django_filters.BooleanFilter(method='filter_in_stock')
+    name = django_filters.CharFilter(field_name="name", lookup_expr='icontains')
+    category_name = django_filters.CharFilter(field_name="category__name", lookup_expr='icontains')
 
     class Meta:
         model = Product
-        fields = ['category', 'min_price', 'max_price', 'in_stock']
+        fields = ['category', 'category_name', 'name', 'min_price', 'max_price', 'in_stock']
 
     def filter_in_stock(self, queryset, name, value):
         if value:
--- ecommerce_app/users/views.py
+++ ecommerce_app/users/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
